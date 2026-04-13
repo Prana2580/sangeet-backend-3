c118c9cf6bfcef5d913840262d162b4ef23dfded
Your Name <infowhatsaup@gmail.com>
2026-04-13 19:52:39 +0530

    adding cors 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ require("./config/passport");
 connectDB();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:5500/"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:5500", "https://sangeet-3-backend.onrender.com"],
   }),
 );
 app.use(express.json());
